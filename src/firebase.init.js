9bd1d09813080e4a2fcb1e1eaffe2b2fcf07e0a4
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-04-09 09:35:29 +0600

    header and firebase

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDaixH1UoZyYAXDK38zBXpTKs34ar_pmSc",
+  authDomain: "react-firebase-intragati-20df9.firebaseapp.com",
+  projectId: "react-firebase-intragati-20df9",
+  storageBucket: "react-firebase-intragati-20df9.appspot.com",
+  messagingSenderId: "750319643282",
+  appId: "1:750319643282:web:8726ecaf8d741f65d8144b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
